d844528df2b1c954c0968f1f164dd3ebc96b7973
Madhavi Perera <127579706+madhavi-pk-perera@users.noreply.github.com>
2023-08-31 03:49:59 +0530

    Add files via upload

---
+++ dicegame_model/Dice__Game/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Dice__Game
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int playerRandomNumber1;
+                int playerRandomNumber2;
+                int enemyRandomNumber1;
+                int enemyRandomNumber2;
+                int playerPoint = 0;
+                int enemyPoint = 0;
+                int playerTotalScore = 0;
+                int enemyTotalScore = 0;
+
+                Random random = new Random();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine("Press enter key to roll the dice...");
+                    Console.ReadKey();
+
+                    if (playerTotalScore >= 100)
+                    {
+                        Console.WriteLine("....Player A has won the game!!!.....");
+                        break;
+                    }
+                    if (enemyTotalScore >= 100)
+                    {
+                        Console.WriteLine("....Player B has won the game!!!.....");
+                        break;
+                    }
+
+
+                    playerRandomNumber1 = random.Next(1, 7);
+                    playerRandomNumber2 = random.Next(1, 7);
+                    playerPoint = playerRandomNumber1 + playerRandomNumber2;
+                    playerTotalScore = playerTotalScore + playerPoint;
+
+
+
+
+                    if (playerRandomNumber1 == playerRandomNumber2)
+                    {
+                        if (playerRandomNumber1 > 1 && playerRandomNumber2 > 1)
+                        {
+                            Console.WriteLine("Dice 1 is : " + playerRandomNumber1);
+                            Console.WriteLine("Dice 2 is : " + playerRandomNumber2);
+                            Console.WriteLine("Score of player A is : " + playerTotalScore);
+                            Console.WriteLine();
+
+
+                            
+
+                            do
+                            {
+                                playerRandomNumber1 = random.Next(1, 7);
+                                playerRandomNumber2 = random.Next(1, 7);
+                                playerPoint = playerRandomNumber1 + playerRandomNumber2;
+                                playerTotalScore = playerTotalScore + playerPoint;
+
+                                Console.WriteLine("Dice 1 is : " + playerRandomNumber1);
+                                Console.WriteLine("Dice 2 is : " + playerRandomNumber2);
+
+                                if (playerRandomNumber1 == 1 && playerRandomNumber2 == 1)
+                                {
+                                    playerTotalScore = 0;
+                                    Console.WriteLine("Score of player A is : " + playerTotalScore);
+                                    Console.WriteLine();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Score of player A is : " + playerTotalScore);
+                                    Console.WriteLine();
+                                }
+
+
+
+                            }
+                            while (playerRandomNumber1 == playerRandomNumber2);
+
+
+
+
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Dice 1 is : " + playerRandomNumber1);
+                            Console.WriteLine("Dice 2 is : " + playerRandomNumber2);
+                            playerTotalScore = 0;
+                            Console.WriteLine("Score of player A is : " + playerTotalScore);
+                            Console.WriteLine();
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dice 1 is : " + playerRandomNumber1);
+                        Console.WriteLine("Dice 2 is : " + playerRandomNumber2);
+                        Console.WriteLine("Score of player A is : " + playerTotalScore);
+                        Console.WriteLine();
+                    }
+
+                    enemyRandomNumber1 = random.Next(1, 7);
+                    enemyRandomNumber2 = random.Next(1, 7);
+                    enemyPoint = enemyRandomNumber1 + enemyRandomNumber2;
+                    enemyTotalScore = enemyTotalScore + enemyPoint;
+
+                    if (enemyRandomNumber1 == enemyRandomNumber2)
+                    {
+                        if (enemyRandomNumber1 > 1 && enemyRandomNumber2 > 1)
+                        {
+                            Console.WriteLine("Dice 1 is : " + enemyRandomNumber1);
+                            Console.WriteLine("Dice 2 is : " + enemyRandomNumber2);
+                            Console.WriteLine("Score of player B is : " + enemyTotalScore);
+                            Console.WriteLine();
+
+                            do
+                            {
+                                enemyRandomNumber1 = random.Next(1, 7);
+                                enemyRandomNumber2 = random.Next(1, 7);
+                                enemyPoint = enemyRandomNumber1 + enemyRandomNumber2;
+                                enemyTotalScore = enemyTotalScore + enemyPoint;
+
+                                Console.WriteLine("Dice 1 is : " + enemyRandomNumber1);
+                                Console.WriteLine("Dice 2 is : " + enemyRandomNumber2);
+
+                                if (enemyRandomNumber1 == 1 && enemyRandomNumber2 == 1)
+                                {
+                                    enemyTotalScore = 0;
+                                    Console.WriteLine("Score of player B is : " + enemyTotalScore);
+                                    Console.WriteLine();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Score of player B is : " + enemyTotalScore);
+                                    Console.WriteLine();
+                                }
+
+
+
+                            }
+                            while (enemyRandomNumber1 == enemyRandomNumber2);
+
+
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Dice 1 is : " + enemyRandomNumber1);
+                            Console.WriteLine("Dice 2 is : " + enemyRandomNumber2);
+                            enemyTotalScore = 0;
+                            Console.WriteLine("Score of player B is : " + enemyTotalScore);
+                            Console.WriteLine();
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dice 1 is : " + enemyRandomNumber1);
+                        Console.WriteLine("Dice 2 is : " + enemyRandomNumber2);
+                        Console.WriteLine("Score of player B is : " + enemyTotalScore);
+                        Console.WriteLine();
+                    }
+
+
+
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+    
+
---
+++ dicegame_model/Dice__Game/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dice__Game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dice__Game")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b469179-0477-4e5f-ae83-1388282c5579")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
